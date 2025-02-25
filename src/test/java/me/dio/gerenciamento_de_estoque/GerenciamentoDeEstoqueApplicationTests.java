530e9b1d9209b164786f6f78bda1a910cd0cf021
Silas Santos <silascruzdossantos@gmail.com>
2025-02-25 20:03:36 -0300

    Initial commit

---
+++ src/test/java/me/dio/gerenciamento_de_estoque/GerenciamentoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.gerenciamento_de_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
